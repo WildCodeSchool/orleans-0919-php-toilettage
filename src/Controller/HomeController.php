d23c34e2a0dbd5c30590e9c74e2eaf1ef580c6d9
Barbara Carmé <barbara_carme@yahoo.fr>
2019-11-06 10:54:20 +0100

    pages mentiosn légales finies avec css

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,4 +21,53 @@ class HomeController extends AbstractController
     {
         return $this->twig->render('Home/index.html.twig');
     }
+
+
+//    public function success()
+//
+//    {
+//        $message= [];
+//        return $this->twig->render('Home/index.html.twig');
+//    }
+
+    public function send()
+    {
+        $errors = [];
+        $success = ['Envoyé'];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $data = array_map('trim', $_POST);
+            if (empty($data['lastname'])) {
+                $errors['lastname'] [] = "Veuillez remplir le champ nom";
+            }
+            if (empty($data['firstname'])) {
+                $errors['firstname'] [] = "Veuillez remplir le champ prénom";
+            }
+            if (empty($data['mail'])) {
+                $errors['mail'] [] = "Veuillez remplir le champ email";
+            } else {
+                if (!filter_var($data['mail'], FILTER_VALIDATE_EMAIL))
+                    $error['mail'] [] = "Format invalide d'email";
+            }
+
+            if (empty($data['message'])) {
+                $errors['message'] [] = "Veuillez remplir le champ message";
+            }
+
+            if (isset($errors)){
+            return $this->twig->render('Home/index.html.twig', [
+                'errors' => $errors,]);
+
+            }
+
+            if (empty($errors))
+            {
+                return $this->twig->render('Success/success.html.twig');
+            }
+
+
+
+        }
+    }
 }
---
+++ src/Controller/MentionController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 11/10/17
+ * Time: 16:07
+ * PHP version 7
+ */
+
+namespace App\Controller;
+
+use App\Model\ItemManager;
+
+/**
+ * Class ItemController
+ *
+ */
+class MentionController extends AbstractController
+{
+
+
+    /**
+     * Display item listing
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+
+        return $this->twig->render('Mention/index.html.twig');
+    }
+
+
+}
