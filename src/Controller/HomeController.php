d449c1ca5a6f17e93299299f81947abdcb6a8416
Barbara Carmé <barbara_carme@yahoo.fr>
2019-11-08 16:30:59 +0100

    securite formulaire en place

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,35 +16,36 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        return $this->twig->render('Home/index.html.twig');
-    }
-
-    public function send()
-    {
-        $errors = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $data = array_map('trim', $_POST);
-            if (empty($data['lastname'])) {
-                $errors['lastname'] = "Veuillez remplir le champ nom";
-            }
-            if (empty($data['firstname'])) {
-                $errors['firstname'] = "Veuillez remplir le champ prénom";
-            }
-            if (empty($data['mail'])) {
-                $errors['mail'] = "Veuillez remplir le champ email";
-            } elseif (!filter_var($data['mail'], FILTER_VALIDATE_EMAIL)) {
-                $errors['mail'] = "Format invalide d'email";
-            }
-            if (empty($data['message'])) {
-                $errors['message'] = "Veuillez remplir le champ message";
-            }
-            if (!empty($errors)) {
-                return $this->twig->render('Home/index.html.twig', [
-                    'errors' => $errors,]);
-            } else {
-                return $this->twig->render('Home/index.html.twig', [
-                    'success' => true]);
+            $errors = $this->validateContact($data);
+
+            if (empty($errors)) {
+                header('Location: /Home/index/?success=ok#contact');
             }
         }
+        return $this->twig->render('Home/index.html.twig', ['errors' => $errors ?? [], 'success' => $_GET['success'] ?? null]);
     }
+
+    private function validateContact($data): array
+    {
+        $errors = [];
+        if (empty($data['lastname'])) {
+            $errors['lastname'] = "Veuillez remplir le champ nom";
+        }
+        if (empty($data['firstname'])) {
+            $errors['firstname'] = "Veuillez remplir le champ prénom";
+        }
+        if (empty($data['mail'])) {
+            $errors['mail'] = "Veuillez remplir le champ email";
+        } elseif (!filter_var($data['mail'], FILTER_VALIDATE_EMAIL)) {
+            $errors['mail'] = "Format invalide d'email";
+        }
+        if (empty($data['message'])) {
+            $errors['message'] = "Veuillez remplir le champ message";
+        }
+
+        return $errors;
+    }
+
 }
