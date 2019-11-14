86026b185e3fa2f99983d9c324146a9b4b68bbcf
Toua VA <toua.va45@gmail.com>
2019-11-14 18:00:58 +0100

    files about CREUD Edit

--- src/Controller/RaceController.php
+++ src/Controller/RaceController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Model\RaceManager;
-
+use App\Model\CategoryManager;
 /**
  * Class RaceController
  *
@@ -23,5 +23,52 @@ class RaceController extends AbstractController
         $raceManager = new RaceManager();
         $races = $raceManager->selectAllOrder();
         return $this->twig->render('Race/index.html.twig', ['races' => $races]);
+    }
+
+    public function edit(int $id): string
+    {
+        $errors = [];
+        $raceManager = new RaceManager();
+        $race = $raceManager->selectOneById($id);
+
+        $categoryManager = new CategoryManager();
+        $categories = $categoryManager->selectAll();
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = array_map('trim', $_POST);
+
+            $errors = $this->validate($data);
+
+            if (empty($errors)) {
+                // update en bdd si pas d'erreur
+                $raceManager->update($data);
+                // redirection en GET
+                header('Location: /Race/edit/' . $id);
+            }
+        }
+
+        return $this->twig->render('Race/edit.html.twig', [
+            'race'  => $race,
+            'data'  => $data ?? [],
+            'errors' => $errors,
+            'categories' => $categories,
+        ]);
+    }
+
+    private function validate (array $data) :array
+    {
+        // verif coté serveur
+        if (empty($data['name'])) {
+            $errors['name'] = 'Veuillez entrer un nom';
+        } elseif (strlen($data['name']) > 80) {
+            $errors['name'] = 'le nom est trop long';
+        }
+
+        if (empty($data['prix'])) {
+            $errors['size'] = 'Un prix est requis';
+        } elseif ($data['size'] < 0) {
+            $errors['size'] = 'le prix doit être positif';
+        }
+        return $errors ?? [];
     }
 }
---
+++ src/Model/CategoryManager.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 07/03/18
+ * Time: 18:20
+ * PHP version 7
+ */
+
+namespace App\Model;
+
+/**
+ * Class CategoryManager
+ * @package Model
+ */
+class CategoryManager extends AbstractManager
+{
+
+    /**
+     *
+     */
+    const TABLE = 'category';
+
+
+    /**
+     * BeastManager constructor.
+     * @param \PDO $pdo
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+}
