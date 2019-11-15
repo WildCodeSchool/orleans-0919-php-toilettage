772ecacb15d48a35676b39c2e83cac939b58b81c
Alexandre Coulon <alex97.coul@gmail.com>
2019-11-15 16:47:39 +0100

    Sprint 3 Pricelist page

---
+++ src/Controller/PricelistController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\RaceManager;
+
+class PricelistController extends AbstractController
+{
+
+    public function index($raceId = null)
+    {
+
+        $animalManager = new RaceManager();
+        $allInAnimals = $animalManager->selectAllInAnimals();
+
+
+        foreach ($allInAnimals as $animal) {
+            $groupedAnimals[$animal['animal']][$animal['category']][] =
+                ['race_name' => $animal['race_name'], 'id' => $animal['id'], 'price' => $animal['price']];
+        }
+
+        if ($raceId) {
+            $raceManager = new RaceManager();
+            $race = $raceManager->selectOneById($raceId);
+            $straighteningPrice = $race['price'] + 50;
+        }
+
+        return $this->twig->render('Pricelist/index.html.twig', [
+            'groupedAnimals' => $groupedAnimals ?? [],
+            'race' => $race ?? ['price' => '--'],
+            'straighteningPrice' => $straighteningPrice ?? '--'
+        ]);
+    }
+}
--- src/Model/RaceManager.php
+++ src/Model/RaceManager.php
@@ -14,4 +14,12 @@ class RaceManager extends AbstractManager
     {
         parent::__construct(self::TABLE);
     }
+
+    public function selectAllInAnimals(): array
+    {
+        $query = "SELECT r.name race_name, r.id, price, c.name category, a.name animal FROM " . self::TABLE . " r
+                    JOIN category c ON c.id=r.category_id
+                        JOIN animal a ON a.id=c.animal_id";
+        return $this->pdo->query($query)->fetchAll();
+    }
 }
