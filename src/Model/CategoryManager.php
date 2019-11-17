098a0fcff3d7308977d897aa696f7b18a767a9d8
Barbara Carmé <barbara_carme@yahoo.fr>
2019-11-17 19:08:46 +0100

    fix on my presentation part on homepage, change color button, div deleted by mistake re-added, link to page done

--- src/Model/CategoryManager.php
+++ src/Model/CategoryManager.php
@@ -22,4 +22,16 @@ class CategoryManager extends AbstractManager
     {
         parent::__construct(self::TABLE);
     }
+
+    public function update(array $data)
+    {
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . "
+                SET name=:name, animal_id=:animal_id           
+                WHERE id=:id
+            ");
+        $statement->bindValue('name', $data['name'], \PDO::PARAM_STR);
+        $statement->bindValue('animal_id', $data['animal_id'], \PDO::PARAM_INT);
+        $statement->bindValue('id', $data['id'], \PDO::PARAM_INT);
+        $statement->execute();
+    }
 }
