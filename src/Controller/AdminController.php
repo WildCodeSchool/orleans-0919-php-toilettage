b27dffd4cf0049db6b19328261ac6f7b1542f6a4
Alexandre Coulon <alex97.coul@gmail.com>
2019-11-18 11:23:39 +0100

    Travis error

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,12 +3,10 @@
 
 namespace App\Controller;
 
-
 class AdminController extends AbstractController
 {
     public function index()
     {
         return $this->twig->render('Admin/index.html.twig');
     }
-
 }
